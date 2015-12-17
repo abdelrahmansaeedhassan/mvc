b192cbe8d985a241df418b94389bf7c72e24d5d2
Ryan Nowak <nowakra@gmail.com>
2015-12-17 14:33:23 -0800

    Smarter antiforgery
    Adds the concept of an IAntiforgeryPolicy marker interface as well as
    the ability to overide policy with a 'closer' filter.
    
    Adds a new [IgnoreAntiforgeryToken] attribute for overriding a scoped
    antiforgery policy.
    
    Adds a new [AutoValidateAntiforgeryToken] attribute (good name tbd) for
    applying an application-wide antiforgery token. The idea is that you can
    configure this as a global filter if your site is acting as a pure
    browser-based or 1st party SPA. This new attribute only validates the
    token for unsafe HTTP methods, so you can apply it broadly.

---
+++ src/Microsoft.AspNet.Mvc.ViewFeatures/AutoValidateAntiforgeryTokenAttribute.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNet.Mvc.ViewFeatures
+{
+    /// <summary>
+    /// An attribute that causes validation of antiforgery tokens for all unsafe HTTP methods.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="AutoValidateAntiforgeryTokenAttribute"/> can be applied at as a global filter to trigger
+    /// validation of antiforgery tokens by default for an application. Use
+    /// <see cref="IgnoreAntiforgeryTokenAttribute"/> to suppress validation of the antiforgery token for
+    /// a controller or action.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class AutoValidateAntiforgeryTokenAttribute : Attribute, IFilterFactory, IOrderedFilter
+    {
+        /// <inheridoc />
+        public int Order { get; set; }
+
+        /// <inheridoc />
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            return serviceProvider.GetRequiredService<AutoValidateAntiforgeryTokenAuthorizationFilter>();
+        }
+    }
+}
--- src/Microsoft.AspNet.Mvc.ViewFeatures/ValidateAntiForgeryTokenAttribute.cs
+++ src/Microsoft.AspNet.Mvc.ViewFeatures/ValidateAntiForgeryTokenAttribute.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNet.Antiforgery;
 using Microsoft.AspNet.Mvc.Filters;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,8 +23,7 @@ namespace Microsoft.AspNet.Mvc
 
         public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
         {
-            var antiforgery = serviceProvider.GetRequiredService<IAntiforgery>();
-            return new ValidateAntiforgeryTokenAuthorizationFilter(antiforgery);
+            return serviceProvider.GetRequiredService<ValidateAntiforgeryTokenAuthorizationFilter>();
         }
     }
 }
